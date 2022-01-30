a6701741ed9b6046b7aa2d36ff757af31c67367f
dfassf <dfassf@gmail.com>
2022-01-30 18:00:22 +0900

    0130

--- lvl2/땅따먹기.js
+++ lvl2/땅따먹기.js
@@ -1,17 +1,23 @@
 function solution(land) {
     let score = 0
-    let currentIndex = getMaxNum(land[0],-1)[1]
-    for(let i = 0; i<land.length; i++){
-        score += getMaxNum(land[i],currentIndex)[0]
+    let currentIndex = getMaxNum(land[0],0, true)[1]
+    score += getMaxNum(land[0],0, true)[0]
+    for(let i = 1; i<land.length; i++){
+        let addNum = getMaxNum(land[i],currentIndex, false)[0]
+        score += addNum
+        currentIndex = getMaxNum(land[i],currentIndex, false)[1]
     }
     return score
 }
 
-function getMaxNum(arr, idx){
+function getMaxNum(arr, idx, isFirst){
     let maxNum = arr[0]
     let index = 0
     for(let i = 1; i<arr.length; i++){
-        if(arr[i]>maxNum && i !== idx){
+        if(arr[i]>maxNum && i !== idx && isFirst == false){
+            maxNum = arr[i]
+            index = i
+        } else if(arr[i]>maxNum && isFirst == true){
             maxNum = arr[i]
             index = i
         }
@@ -19,4 +25,4 @@ function getMaxNum(arr, idx){
     return [maxNum, index]
 }
 
-// console.log(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
+console.log(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
