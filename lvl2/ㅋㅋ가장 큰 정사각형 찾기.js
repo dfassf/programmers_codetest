a27a5e560aac0d5dc7c643f282c264dc49c24d3f
Shinwoo Um <dfassf@gmail.com>
2022-03-01 16:43:06 +0900

    0301

--- lvl2/ㅋㅋ가장 큰 정사각형 찾기.js
+++ lvl2/ㅋㅋ가장 큰 정사각형 찾기.js
@@ -1,12 +1,24 @@
 function solution(board)
 {
-    var answer = 1234;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
+    // 1이 연속해서 있는 길이
+    // 각 행에 대해 시행
+    let arr = []
+    for(let i = 0; i < board.length; i++){
+        let lengthArr = []
+        let lengthObj = {start: 0, end: 0}
+        let cnt = 0
+        for(let j = 0; j<board[i].length; j++){
+            if(board[i][j] == 0 && cnt == 0){
+                cnt = 0
+            } else if(board[i][j] == 0 && cnt !== 0){
+                
+            } else{ cnt ++}
+        }
+        arr.push(cnt)
+    }
+    console.log(arr)
 }
 
-let input = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+// let input = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+let input = [[0,0,1,1],[1,1,1,1]]
 solution(input)
