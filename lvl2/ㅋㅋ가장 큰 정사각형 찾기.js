f0c73dac1817d2e35f983fd0f8370b64be2d3f1f
Shinwoo Um <dfassf@gmail.com>
2022-03-01 16:31:20 +0900

    0301

---
+++ lvl2/ㅋㅋ가장 큰 정사각형 찾기.js
@@ -0,0 +1,12 @@
+function solution(board)
+{
+    var answer = 1234;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return answer;
+}
+
+let input = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+solution(input)
